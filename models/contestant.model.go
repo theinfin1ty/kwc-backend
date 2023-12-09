8e894492676028541eac3700d6cfaa872f4cc46c
theinfin1ty <anantgaurav1010@gmail.com>
2023-12-10 00:45:39 +0530

    chore: update contestants model

--- models/contestant.model.go
+++ models/contestant.model.go
@@ -2,7 +2,7 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type Socials struct {
+type Social struct {
 	Id   primitive.ObjectID `bson:"_id" json:"id"`
 	Name string             `bson:"name" json:"name"`
 	Url  string             `bson:"url" json:"url"`
@@ -12,5 +12,5 @@ type Contestant struct {
 	Id      primitive.ObjectID `bson:"_id" json:"id"`
 	Name    string             `bson:"name" json:"name"`
 	Image   string             `bson:"image" json:"image"`
-	Socials []Socials          `bson:"socials" json:"socials"`
+	Socials []Social           `bson:"socials" json:"socials"`
 }
