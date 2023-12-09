bd08e52135cd53a5bd401046af5152b2e230a341
theinfin1ty <anantgaurav1010@gmail.com>
2023-12-10 00:51:02 +0530

    chore: update contestant routes

--- routes/contestant.route.go
+++ routes/contestant.route.go
@@ -8,9 +8,9 @@ import (
 )
 
 func ContestantRoutes(app *gin.Engine) {
-	app.POST("/api/contestant", middlewares.Auth([]string{"admin"}), controllers.AddContestant)
-	app.GET("/api/contestant", controllers.ListContestants)
-	app.GET("/api/contestant/:id", controllers.GetContestant)
-	app.PATCH("/api/contestant/:id", middlewares.Auth([]string{"admin"}), controllers.UpdateContestant)
-	app.DELETE("/api/contestant/:id", middlewares.Auth([]string{"admin"}), controllers.DeleteContestant)
+	app.POST("/api/contestants", middlewares.Auth([]string{"admin"}), controllers.AddContestant)
+	app.GET("/api/contestants", controllers.ListContestants)
+	app.GET("/api/contestants/:id", controllers.GetContestant)
+	app.PATCH("/api/contestants/:id", middlewares.Auth([]string{"admin"}), controllers.UpdateContestant)
+	app.DELETE("/api/contestants/:id", middlewares.Auth([]string{"admin"}), controllers.DeleteContestant)
 }
