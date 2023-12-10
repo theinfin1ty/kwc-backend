22dfddd5c2fbb61fe8d71faf509e31ddb16e1fbd
theinfin1ty <anantgaurav1010@gmail.com>
2023-12-10 20:31:21 +0530

    chore: changed go version and updated question model

--- models/question.model.go
+++ models/question.model.go
@@ -3,11 +3,11 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Question struct {
-	Id        primitive.ObjectID `bson:"_id" json:"id"`
-	Question  string             `bson:"question" json:"question"`
-	Answers   []string           `bson:"answers" json:"answers"`
-	Images    []string           `bson:"images" json:"images"`
-	Hints     []string           `bson:"hints" json:"hints"`
-	EpisodeId primitive.ObjectID `bson:"episodeId" json:"episodeId"`
-	Episode   Episode            `json:"episode"`
+	Id        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Question  string             `bson:"question,omitempty" json:"question"`
+	Answers   []string           `bson:"answers,omitempty" json:"answers"`
+	Images    []string           `bson:"images,omitempty" json:"images"`
+	Hints     []string           `bson:"hints,omitempty" json:"hints"`
+	EpisodeId primitive.ObjectID `bson:"episodeId,omitempty" json:"episodeId"`
+	Episode   []Episode          `bson:"episode,omitempty" json:"episode"`
 }
