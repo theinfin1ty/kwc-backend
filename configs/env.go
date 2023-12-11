4cf891170e7488cbe9d04252a4e20630a4c3ada3
theinfin1ty <anantgaurav1010@gmail.com>
2023-12-11 23:39:20 +0530

    remove env file error handling

--- configs/env.go
+++ configs/env.go
@@ -1,16 +1,15 @@
 package configs
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func GetEnvVariable(key string) string {
-	err := godotenv.Load(".env")
-	if err != nil {
-		fmt.Println("Error loading .env file")
-	}
+	_ = godotenv.Load(".env")
+	// if err != nil {
+	// 	fmt.Println("Error loading .env file")
+	// }
 	return os.Getenv(key)
 }
